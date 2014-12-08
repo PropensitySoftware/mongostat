279968b7a58f1db15a8f8c06fb75e9231dcc3ea0
Matt Nuss <mnuss@niyamit.com>
2014-12-07 19:01:52 -0500

    Updating config

--- config/env/test.js
+++ config/env/test.js
@@ -13,7 +13,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Test Environment'
+		title: 'Mongostat - Test Environment'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
